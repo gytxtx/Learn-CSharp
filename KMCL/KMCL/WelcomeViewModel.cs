359b72536f566d65110f52941b59ae6085c98eb3
gytxtx <gytxtx@outlook.com>
2025-02-15 16:47:13 +0800

    测试设置向导窗口关闭时的对话框

---
+++ KMCL/KMCL/WelcomeViewModel.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using MaterialDesignThemes.Wpf;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassIsland.ViewModels;
+
+public class WelcomeViewModel : ObservableRecipient
+{
+    private int _slideIndex = 0;
+    private bool _isExitConfirmed = false;
+    private int _masterTabIndex = 0;
+    private int _flipNextCount = 0;
+    private bool _isFlipEnd = false;
+    private int _flipIndex = 0;
+    private bool _createStartupShortcut = true;
+    private bool _createStartMenuShortcut = true;
+    private bool _createDesktopShortcut = false;
+    private int _slideIndexMaster = 0;
+    private bool _registerUrlScheme = false;
+    private bool _createClassSwapShortcut = false;
+    private bool _requiresRestarting = false;
+    private DateTime _singleWeekStartTime = DateTime.Now;
+
+
+    // 从 Guid 改为 string 类型
+    private string _dialogId = Guid.NewGuid().ToString();
+    public string DialogId
+    {
+        get => _dialogId;
+        set => SetProperty(ref _dialogId, value);
+    }
+
+    // public Guid DialogId
+    // {
+    //    get;
+    // } = Guid.NewGuid();
+
+    public int MasterTabIndex
+    {
+        get => _masterTabIndex;
+        set
+        {
+            if (value == _masterTabIndex) return;
+            _masterTabIndex = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int SlideIndex
+    {
+        get => _slideIndex;
+        set
+        {
+            if (value == _slideIndex) return;
+            _slideIndex = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int SlideIndexMaster
+    {
+        get => _slideIndexMaster;
+        set
+        {
+            if (value == _slideIndexMaster) return;
+            _slideIndexMaster = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+
+    public bool IsExitConfirmed
+    {
+        get => _isExitConfirmed;
+        set
+        {
+            if (value == _isExitConfirmed) return;
+            _isExitConfirmed = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int FlipNextCount
+    {
+        get => _flipNextCount;
+        set
+        {
+            if (value == _flipNextCount) return;
+            _flipNextCount = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsFlipEnd
+    {
+        get => _isFlipEnd;
+        set
+        {
+            if (value == _isFlipEnd) return;
+            _isFlipEnd = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int FlipIndex
+    {
+        get => _flipIndex;
+        set
+        {
+            if (value == _flipIndex) return;
+            _flipIndex = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool CreateStartupShortcut
+    {
+        get => _createStartupShortcut;
+        set
+        {
+            if (value == _createStartupShortcut) return;
+            _createStartupShortcut = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool CreateStartMenuShortcut
+    {
+        get => _createStartMenuShortcut;
+        set
+        {
+            if (value == _createStartMenuShortcut) return;
+            _createStartMenuShortcut = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool CreateDesktopShortcut
+    {
+        get => _createDesktopShortcut;
+        set
+        {
+            if (value == _createDesktopShortcut) return;
+            _createDesktopShortcut = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool RegisterUrlScheme
+    {
+        get => _registerUrlScheme;
+        set
+        {
+            if (value == _registerUrlScheme) return;
+            _registerUrlScheme = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool CreateClassSwapShortcut
+    {
+        get => _createClassSwapShortcut;
+        set
+        {
+            if (value == _createClassSwapShortcut) return;
+            _createClassSwapShortcut = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool RequiresRestarting
+    {
+        get => _requiresRestarting;
+        set
+        {
+            if (value == _requiresRestarting) return;
+            _requiresRestarting = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public DateTime SingleWeekStartTime
+    {
+        get => _singleWeekStartTime;
+        set
+        {
+            if (value.Equals(_singleWeekStartTime)) return;
+            _singleWeekStartTime = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public SnackbarMessageQueue SnackbarQueue { get; } = new();
+}
--- KMCL/KMCL/Window_Test.xaml.cs
+++ KMCL/KMCL/Window_Test.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ClassIsland.ViewModels;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +23,44 @@ namespace KMCL
     /// </summary>
     public partial class Window_Test : Window
     {
+        public WelcomeViewModel ViewModel { get; } = new();
+
         public Window_Test()
         {
             InitializeComponent();
+            MessageBox.Show($"ViewModel.DialogId: {ViewModel.DialogId}");
+            DataContext = ViewModel; // 必须设置 DataContext
+        }
+        //public WelcomeViewModel ViewModel
+        //{
+        //    get;
+        //    set;
+        //} = new();
+
+
+        // private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        // private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        private async void Window_Closing(object? sender, CancelEventArgs e)
+        {
+            if (ViewModel.IsExitConfirmed) return;
+
+            e.Cancel = true;
+
+            // 使用 ViewModel 中的动态 DialogId
+            var dialogId = ViewModel.DialogId;
+
+            if (DialogHost.IsDialogOpen(dialogId)) return;
+
+            var result = await DialogHost.Show(
+                FindResource("ExitAppConfirmDialog"),
+                dialogId // 使用绑定的动态标识符
+            );
+
+            if (result is true)
+            {
+                ViewModel.IsExitConfirmed = true;
+                Close();
+            }
         }
     }
 }
